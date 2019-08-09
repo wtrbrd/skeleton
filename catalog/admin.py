98930e7c6880bd037bad52da3571f3c3b3968d46
wtrbrd <nick.valdez@wpengine.com>
2019-08-09 16:24:23 -0500

    Admin Page Models

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,63 +1,21 @@
 from django.contrib import admin
+from catalog.models import Author, Genre, Book, BookInstance, Language
+
 
 # Register your models here.
 admin.site.register(Genre)
-admin.site.register(Book)
-admin.site.register(BookInstance)
-admin.site.register(Author)
+#admin.site.register(Book)
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+#admin.site.register(BookInstance)
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
+#admin.site.register(Author)
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
 admin.site.register(Language)
-
-
-
-Redirect 301 ^/supplemental-insurance/	/
-Redirect 301 ^/index.php	/
-Redirect 301 ^/specialty-programs/	/
-Redirect 301 ^/voluntary-programs/	/
-Redirect 301 ^/site-map/	/sitemap/
-Redirect 301 ^/author/fsi/	/
-Redirect 301 ^/author/siderkor/	/
-Redirect 301 ^/employer-benefits/	/
-Redirect 301 ^/hr-360/	/hr360/
-Redirect 301 ^/privacy-statement/	/privacy-policy/
-Redirect 301 ^/terms-of-use/	/privacy-policy/
-Redirect 301 ^/long-term-care/	/liability-insurance/
-Redirect 301 ^/employment-practices-resources/	/articles/
-Redirect 301 ^/2019-survey-results/	/wp-content/uploads/2019/07/bcg-2019-survey-results.pdf
-Redirect 301 ^/hr-360-bcg/	/hr360/
-Redirect 301 ^/balance-point-workforce-management/	/articles/
-Redirect 301 ^/2017-njbankers-member-survey/	/
-Redirect 301 ^/wp-content/uploads/2014/03/disability-brochure.pdf	/important-forms/
-Redirect 301 ^/wp-content/uploads/2014/03/accidents-brochure.pdf	/important-forms/
-Redirect 301 ^/wp-content/uploads/2014/03/universal-life-brochure.pdf	/important-forms/
-Redirect 301 ^/wp-content/uploads/2014/03/critical-illness-brochure.pdf	/important-forms/
-Redirect 301 /category/home-content-middle-middle/	/important-forms/
-Redirect 301 ^/category/home-content-middle-left/	/important-forms/
-Redirect 301 ^/wp-content/uploads/2016/03/Aetna-LTD-Only-Form.pdf	/important-forms/
-Redirect 301 ^/wp-content/uploads/2014/03/Life-Long-Term-Disability.pdf	/important-forms/
-Redirect 301 ^/wp-content/uploads/2016/03/Aetna-Life-Beneficiary-Designation-Form.pdf	/important-forms/
-Redirect 301 ^/wp-content/uploads/2014/03/GL2003090-Group-Portability-Election-Form.pdf	/important-forms/
-Redirect 301 ^/wp-content/uploads/2016/03/Aetna-Life-Insurance-Only-Form.pdf	/important-forms/
-Redirect 301 ^/important-forms/sar-070116-063017-2/	/important-forms/
-Redirect 301 ^/wp-content/uploads/2016/06/NJBankers_Member_2016_Healthcare_Survey_20161.pdf	/important-forms/
-Redirect 301 ^/2017-njbankers-member-survey/2018-survey-results/	/important-forms/
-Redirect 301 ^/bcg-patronage-dividend-thank-you-to-our-shareholders/	/articles/
-Redirect 301 ^/wp-content/uploads/2014/03/Term-Form.pdf	/important-forms/
-Redirect 301 ^/2017-njbankers-member-survey/2017-njbankers-member-survey/	/important-forms/
-Redirect 301 ^/wp-content/uploads/2014/03/Conversion-Rates-2014.pdf	/important-forms/
-Redirect 301 ^/wp-content/uploads/2014/03/NJB-App-Fillable.pdf	/important-forms/
-Redirect 301 ^/wp-content/uploads/2018/05/2018-Survey-Results.pdf	/important-forms/
-Redirect 301 ^/wp-content/uploads/2014/03/marketing-results-summary.pdf	/important-forms/
-Redirect 301 ^/wp-content/uploads/2014/03/SAR-070115-063016f.pdf	/important-forms/
-Redirect 301 ^/wp-content/uploads/2019/04/2019-survey-results.pdf	/important-forms/
-Redirect 301 ^/budget-and-tax-deal-delays-excise-tax-to-2020/	/important-forms/
-Redirect 301 ^/wp-content/uploads/2016/06/2017-NJBankers-Member-Survey.pdf	/important-forms/
-Redirect 301 ^/wp-content/uploads/2016/03/Aetna-Life-Enrollment-Form.pdf	/important-forms/
-Redirect 301 ^/wp-content/uploads/2014/03/long-term-care-brochure.pdf	/important-forms/
-Redirect 301 ^/no-rate-increase-again-for-njbankers-sponsored-delta-dental-program/	/important-forms/
-Redirect 301 ^/wp-content/uploads/2016/04/SAR-070113-063015-2016.pdf	/important-forms/
-Redirect 301 ^/wp-content/uploads/2014/03/Term-Form-Dependents-Only.pdf	/important-forms/
-Redirect 301 ^/wp-content/uploads/2014/03/Salary-Change-Form.pdf	/important-forms/
-Redirect 301 ^/wp-content/uploads/2014/03/Notice-of-Group-Life-Conversion-2014.pdf	/important-forms/
-Redirect 301 ^/wp-content/uploads/2014/02/Talking-to-Children-EAP-CE.pdf	/important-forms/
-Redirect 301 ^/wp-content/uploads/2014/03/BCG-Outsourcing-Enrollment-Card-Fillable.pdf	/important-forms/
-Redirect 301 /wp-content/uploads/2014/02/Dealing-with-Difficult-People-EAP-CE.pdf	/important-forms/
